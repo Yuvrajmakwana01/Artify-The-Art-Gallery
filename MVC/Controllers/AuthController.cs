89c85c986fb32a424f1600483fda5f005ff158a0
Vin8154 <cse.220840131062@gmail.com>
2026-04-10 15:22:57 +0530

    Landing Page and different buttons to redirect different actions

--- MVC/Controllers/AuthController.cs
+++ MVC/Controllers/AuthController.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace MVC.Controllers
 {
-    // [Route("[controller]")]
     public class AuthController : Controller
     {
         private readonly ILogger<AuthController> _logger;
@@ -17,12 +10,6 @@ namespace MVC.Controllers
         {
             _logger = logger;
         }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
