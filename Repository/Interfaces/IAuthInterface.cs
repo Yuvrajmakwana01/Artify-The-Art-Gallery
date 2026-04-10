98f478159daf5c8999a0f3d46e4df6426e4fe813
dishaa0704 <dishaa0704@gmail.com>
2026-04-10 16:56:19 +0530

    Buyer Side Login and register added (credentials removed)

--- Repository/Interfaces/IAuthInterface.cs
+++ Repository/Interfaces/IAuthInterface.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Repository.Models;
 
 namespace Repository.Interfaces
 {
     public interface IAuthInterface
     {
-        
+        Task<int> UserRegister(t_UserRegister model);
+        Task<t_UserRegister?> UserLogin(t_UserLogin model);
+
+         Task<t_UserRegister?> GetUserByEmail(string email);
+        Task<int> UpdatePassword(string email, string newPassword);
     }
 }
